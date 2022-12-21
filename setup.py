edbdd46b3207be59fa1640558e61ba7cc6ad6cde
Helmut Konrad Schewe <helmutus@outlook.com>
2022-12-21 18:35:31 +0000

    chore(setup): add missing packages

--- setup.py
+++ setup.py
@@ -12,9 +12,25 @@ import utila
 
 PACKAGES = [
     'nltk_data',
+    'nltk_data.chunkers.maxent_ne_chunker',
+    'nltk_data.chunkers.maxent_ne_chunker.PY3',
+    'nltk_data.corpora',
+    'nltk_data.corpora.crubadan',
+    'nltk_data.corpora.names',
+    'nltk_data.corpora.stopwords',
+    'nltk_data.corpora.words',
+    'nltk_data.misc.perluniprops',
+    'nltk_data.taggers.averaged_perceptron_tagger',
+    'nltk_data.taggers.maxent_treebank_pos_tagger',
+    'nltk_data.taggers.maxent_treebank_pos_tagger.PY3',
+    'nltk_data.tokenizers.punkt',
+    'nltk_data.tokenizers.punkt.PY3',
 ]
 
 if __name__ == "__main__":
-    utila.install(__file__)
+    utila.install(
+        __file__,
+        include_package_data=True,
+    )
     import nltk_data.compile
     nltk_data.compile.compile_names()
