6bc1f47ecc2b3a6db9d3007c8b41cf80e24433ca
Helmut Konrad Schewe <helmutus@outlook.com>
2022-12-21 18:08:39 +0000

    chore(test): ensure that nltk_data is installed

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,8 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import utila
+
 pytest_plugins = ['pytester', 'xdist']  # pylint: disable=invalid-name
+
+utila.run('python setup.py build', cwd=utila.baw_root(__file__))
