402860ea9d04a6ca9c5c233a0feb8c1cc8bab02f
Helmut Konrad Schewe <helmutus@outlook.com>
2022-12-21 15:22:22 +0000

    chore(setup): use quick install

--- setup.py
+++ setup.py
@@ -8,44 +8,13 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'nltk_data/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'nltk_data',
+]
 
 if __name__ == "__main__":
-    # allow setup.py to run from another directory
-    os.chdir(ROOT)
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='data data data',
-        include_package_data=True,
-        install_requires=REQUIRES,
-        long_description=README,
-        name='nltk_data',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/nltk_data',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'nltk_data',
-        ],
-    )
+    utila.install(__file__)
     import nltk_data.compile
     nltk_data.compile.compile_names()
