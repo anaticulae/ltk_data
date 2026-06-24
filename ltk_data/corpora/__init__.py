2c6e24e64f3b5e3d1c85c78bfe511af30605b023
Helmut Konrad Schewe <helmutus@outlook.com>
2026-06-24 03:18:58 +0200

    refactor(utilo): use utilo code

--- ltk_data/corpora/__init__.py
+++ ltk_data/corpora/__init__.py
@@ -7,15 +7,15 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
+import utilo
 
 import ltk_data
 
-CORPORA = os.path.join(ltk_data.ROOT, 'ltk_data/corpora')
+CORPORA = utilo.join(ltk_data.ROOT, 'ltk_data/corpora')
 
-NAMES = os.path.join(CORPORA, 'names')
-NAMES_MALE = os.path.join(NAMES, 'male')
-NAMES_FEMALE = os.path.join(NAMES, 'female')
-NAMES_FAMILY = os.path.join(NAMES, 'family')
+NAMES = utilo.join(CORPORA, 'names')
+NAMES_MALE = utilo.join(NAMES, 'male')
+NAMES_FEMALE = utilo.join(NAMES, 'female')
+NAMES_FAMILY = utilo.join(NAMES, 'family')
 
-STOPWORDS = os.path.join(CORPORA, 'stopwords')
+STOPWORDS = utilo.join(CORPORA, 'stopwords')
